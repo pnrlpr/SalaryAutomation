868d533935911c15c59089159ed6137ff4be1c12
pnrlpr <ppinaralper@gmail.com>
2023-06-07 10:57:31 +0300

    addition: portals added

--- sifirdan/src/App.jsx
+++ sifirdan/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import AddWorker from "./components/Workers/AddWorker";
 import WorkerList from "./components/Workers/WorkerList";
+import React from "react";
 
- //! bu projede tim css classları için tailwind kullanıldı
+//! bu projede tim css classları için tailwind kullanıldı
 
 function App() {
-  const [workers, setWorkers]= useState ([]);
+  const [workers, setWorkers] = useState([]);
   return (
-    <div className="App">
+    <React.Fragment>
       {/*  AddWorker aşağıdan seçip Ctrl + space yaparsan otomatik import ediyor */}
       <h1 className="text-white text-center mt-6 text-3xl">Maaş Otomasyonu</h1>
-   <AddWorker setWorkers={setWorkers}/>    
-   <WorkerList workers={workers} setWorkers={setWorkers}/>
-    </div>
+      <AddWorker setWorkers={setWorkers} />
+      <WorkerList workers={workers} setWorkers={setWorkers} />
+    </React.Fragment>
   );
 }
 
--- sifirdan/src/components/UI/ErrorModal.jsx
+++ sifirdan/src/components/UI/ErrorModal.jsx
@@ -1,28 +1,55 @@
 import Button from "./Button";
 import Card from "./Card";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const ErrorModal = (props) => {
-  const { onConfirm, error } = props;
-  const { title, message } = error;
+//Portal uygulamak için önce ErrorModal içindeki Backdrop ve ModalOverlay olarak 2 ayrı komponente böldük!
+//Sonra index.html de backdrop-root id'li yeni div açtık.
+//SOnra ErrorModal içinde reactDOM olan satırı yazdık
+//Bu Backdrop komponentinin hani portalda gösterileceğini belirtir
+
+const Backdrop = (props) => {
+  return (
+    <div
+      className="backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0 left-0"
+      onClick={props.onConfirm}
+    ></div>
+  );
+};
+
+const ModalOverlay = (props) => {
   return (
     <div className="error-modal">
-      <div
-        className="backdrop-blur-sm bg-white/30 fixed w-screen h-screen top-0"
-        onClick={onConfirm}
-      ></div>
       <Card className="w-[36rem] p-0 z-20">
         {/* Card komponentine z-20 yani z indexinde 20 birim öne çıksın dedik. Bu nedenle blur alanın önünde görünüyor! */}
         <header className="bg-red-700 p-4 rounded-t-xl ">
-          <h2 className="text-center text-xl text-white">{title}</h2>
+          <h2 className="text-center text-xl text-white">{props.title}</h2>
         </header>
-        <section className="p-2">{message}</section>
+        <section className="p-2">{props.message}</section>
         <footer className="p-2 flex justify-end">
-          <Button className="rounded w-32" onClick={onConfirm}>
+          <Button className="rounded w-32" onClick={props.onConfirm}>
             Done!
           </Button>
         </footer>
       </Card>
     </div>
+  );
+};
+
+const ErrorModal = (props) => {
+  const { onConfirm, error } = props;
+  const { title, message } = error;
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
